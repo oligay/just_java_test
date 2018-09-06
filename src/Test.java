c5f846f3a673516404a22227c5b84a77d14377a1
MorvanHu <18215583994@163.com>
2018-09-06 10:30:19 +0800

    尝试

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Test {
+    private int anInt;
+    private double j;
+    public Test() {
+    }
+
+    public static void main(String[] args) {
+        new ArrayList<Integer>();
+
+    }
+
+}
